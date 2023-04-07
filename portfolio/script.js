373d009594b525ce9656a736e2a1576748234f4a
Mohdsofi1 <126760135+Mohdsofi1@users.noreply.github.com>
2023-04-08 05:43:53 +0800

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,17 +1,4 @@
-let aboutMeDOM = document.querySelector("#button-1");
-let popupDOM = document.querySelector("#popup-1");
-let closeDOM = document.querySelector(".close-btn");
-
-// aboutMeDOM.addEventListener("click", function () {
-//   popupDOM.style.display = "show";
-//   console.log(aboutMeDOM);
-// });
-
-// closeDOM.addEventListener("click", function () {
-//   popupDOM.style.display = "none";
-//   console.log(closeDOM);
-// });
-
+// code untuk modal aboutme
 document.addEventListener("DOMContentLoaded", function () {
   const aboutMeButton = document.getElementById("aboutMe");
   const modal = document.getElementById("modal");
@@ -24,9 +11,26 @@ document.addEventListener("DOMContentLoaded", function () {
   closeButton.addEventListener("click", function () {
     modal.style.display = "none";
   });
-
   window.addEventListener("click", function (event) {
     if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const myProjectButton = document.getElementById("myProject");
+  const modal = document.getElementById("modal-1");
+  const closeButton = document.querySelector(".close-1");
+
+  myProjectButton.addEventListener("click", function () {
+    modal.style.display = "flex";
+  });
+  closeButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+  window.addEventListener("click", function (event) {
+    if (event.target === myProject) {
       modal.style.display = "none";
     }
   });
